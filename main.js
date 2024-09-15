ae8331b7f638adff6fbe90458c8c18f311280aae
Xandsz <64386219+Lokii808@users.noreply.github.com>
2024-09-15 20:28:28 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const { exec } = require('child_process');
+
+// Executa um comando no servidor
 exec('whoami', (err, stdout, stderr) => {
     if (err) {
         console.error(`Error: ${err.message}`);
